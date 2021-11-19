3996ae4b84dd1fdd016f8b520dad7ad043008987
GitHub Action <action@github.com>
2021-11-19 11:47:52 +0000

    Template cleanup

--- src/main/kotlin/com/github/sasuke007/demoplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/sasuke007/demoplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.sasuke007.demoplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.sasuke007.demoplugin.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/sasuke007/demoplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/sasuke007/demoplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.sasuke007.demoplugin.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.sasuke007.demoplugin.MyBundle
 
 class MyProjectService(project: Project) {
 
